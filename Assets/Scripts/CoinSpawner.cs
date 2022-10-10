93f644bd7943d11f8f7a0af336ed0c1f765f5326
DawidJerdonek <dawid.jerdonek@gmail.com>
2022-10-10 13:31:17 +0100

    Added TMP and merged issues

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    private GameObject coinClone;
+    private float timeLeft;
+    private float timeCoinSpawn = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = timeCoinSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCoinSpawn = Random.Range(2, 6);
+
+
+        //InvokeRepeating("spawnEnemy", 10, 10);
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            spawnEnemy();
+            timeLeft = timeCoinSpawn;
+        }
+
+    }
+
+
+    void spawnEnemy()
+    {
+        //obstacleClone = Instantiate(obstaclePrefab, new Vector2(10.0f,-2.4f), Quaternion.identity);
+
+        MapGen mapgener = GetComponent<MapGen>();
+        int ChunkToRemove = Random.Range(18, mapgener.chunks.Count);
+        Vector3 newPos = mapgener.chunks[ChunkToRemove].GetComponent<WorldChunk>().topTile.position + new Vector3(0, 0.9f, 0);
+
+        coinClone = Instantiate(coinPrefab, newPos, Quaternion.identity, mapgener.chunks[ChunkToRemove].transform);
+
+    }
+}
