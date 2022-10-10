5540d53ae0769c061ba9077e29196eec2640ec22
AedenMoylan <aedenmoylan@gmail.com>
2022-10-10 10:52:24 +0100

    adding basic player functionality

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private GameObject Player;
+    public Rigidbody2D rb;
+    private int jumpSpeed = 7;
+    private float moveSpeed = 3;
+    private bool isJumping = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = this.gameObject;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveLeft();
+        moveRight();
+        jump();
+    }
+
+    public void moveLeft()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }     
+    }
+
+    public void moveRight()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+    }
+
+    public void jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && isJumping == false)
+        {
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;            
+        }
+    }
+}
