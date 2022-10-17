0a41f0691f74ae7d22005bd4d1deca31a4cc4207
David Jalisevs <58332574+DavidJalisevs@users.noreply.github.com>
2022-10-17 12:09:44 +0100

    PlaytestingForm
    Playtesting  form jiraID RWMPC22-30

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,12 +13,50 @@ public class PlayerScript : MonoBehaviour
     private float moveSpeed = 3;
     private bool isJumping = false;
     public int lives = 3;
+    private string deviceID;
+    private string timesDied;
+    private string distTraveled;
+    private string highScore; 
+
+    private float targetTime = 0.0f;
+
+    //https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=IDHERE&entry.1637826786=TIMESDIED&entry.1578808278=HIGHSCORE&entry.2039373689=DISTANCE
+
+
+    [SerializeField]
+    private string BASE_URLLL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/formResponse";
+    private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=";
 
     // Start is called before the first frame update
     void Start()
     {
         Player = this.gameObject;
         rb = GetComponent<Rigidbody2D>();
+        timesDied = "10";
+        distTraveled = "6969";
+        highScore = "9999999";
+    }
+
+
+    IEnumerator Post()//, string email, string phone)
+    {
+        //BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=" + Name;
+
+        WWWForm form = new WWWForm();
+        //form.AddField("entry.51372667", name);
+
+        byte[] rawData = form.data;
+        string url = BASE_URL;
+
+
+
+        // Post a request to an URL with our custom headers
+        WWW www = new WWW(url, rawData);
+        yield return www;
+    }
+    public void Send()
+    {
+        StartCoroutine(Post());
     }
 
     // Update is called once per frame
@@ -27,13 +65,31 @@ public class PlayerScript : MonoBehaviour
         moveLeft();
         moveRight();
         jump();
-        
+        deviceID = uniqueID();
+        BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=" + deviceID + "&entry.1637826786=" + timesDied + "&entry.1578808278=" + highScore + " &entry.2039373689=" + distTraveled;
+
         scoreText.text = "Score: " + score.ToString();
         if (lives <= 0)
 
         {
             Destroy(gameObject);
         }
+
+        targetTime -= Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            if (targetTime <= 0.0f)
+            {
+                Application.OpenURL(BASE_URL);
+                Debug.Log("deviceID" + deviceID);
+
+                Send();
+                targetTime = 5.0f;
+            }
+        }
+
     }
 
     public void moveLeft()
@@ -76,4 +132,19 @@ public class PlayerScript : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+
+
+
+    string uniqueID()
+    {
+    
+        int z1 = UnityEngine.Random.Range(0, 1000000);
+        int z2 = UnityEngine.Random.Range(0, 1000000);
+        string uid =  z1 + ":" + z2;
+        return uid;
+    }
+
+
+
+
 }
