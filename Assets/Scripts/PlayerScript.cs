cf9002d027540d6597eb9f83ea09594bd949d698
David Jalisevs <58332574+DavidJalisevs@users.noreply.github.com>
2022-10-17 13:53:59 +0100

    fixed form

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,18 +13,14 @@ public class PlayerScript : MonoBehaviour
     private float moveSpeed = 3;
     private bool isJumping = false;
     public int lives = 3;
-    private string deviceID;
     private string timesDied;
     private string distTraveled;
     private string highScore; 
 
-    private float targetTime = 0.0f;
-
+   
     //https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=IDHERE&entry.1637826786=TIMESDIED&entry.1578808278=HIGHSCORE&entry.2039373689=DISTANCE
 
 
-    [SerializeField]
-    private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=";
 
     // Start is called before the first frame update
     void Start()
@@ -34,28 +30,11 @@ public class PlayerScript : MonoBehaviour
         timesDied = "10";
         distTraveled = "6969";
         highScore = "9999999";
-        deviceID = uniqueID();
 
     }
 
 
-    IEnumerator Post()//, string email, string phone)
-    {
-
-        WWWForm form = new WWWForm();
-        //form.AddField("entry.51372667", name);
-        byte[] rawData = form.data;
-        string url = BASE_URL;
-
-        // Post a request to an URL with our custom headers
-        WWW www = new WWW(url, rawData);
-        yield return www;
-    }
-    public void Send()
-    {
-        StartCoroutine(Post());
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
@@ -64,8 +43,7 @@ public class PlayerScript : MonoBehaviour
         jump();
         GameManager.instance.livesText.text = "Lives: " + lives.ToString();
         GameManager.instance.scoreText.text = "Score: " + score.ToString();
-        deviceID = uniqueID();
-        BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfdbsO2vKysmX5H7sdABY5K6j155kXHvC_E2SpmcHrQ8XzJpA/viewform?usp=pp_url&entry.51372667=" + deviceID + "&entry.1637826786=" + timesDied + "&entry.1578808278=" + highScore + " &entry.2039373689=" + distTraveled;
+       
 
         //scoreText.text = "Score: " + score.ToString();
         if (lives <= 0)
@@ -74,20 +52,9 @@ public class PlayerScript : MonoBehaviour
             Destroy(gameObject);
         }
 
-        targetTime -= Time.deltaTime;
 
 
-        if (Input.GetKey(KeyCode.L))
-        {
-            if (targetTime <= 0.0f)
-            {
-                Application.OpenURL(BASE_URL);
-                Debug.Log("deviceID" + deviceID);
-
-                Send();
-                targetTime = 5.0f;
-            }
-        }
+      
 
     }
 
@@ -145,14 +112,6 @@ public class PlayerScript : MonoBehaviour
 
 
 
-    string uniqueID()
-    {
-    
-        int z1 = UnityEngine.Random.Range(0, 1000000);
-        int z2 = UnityEngine.Random.Range(0, 1000000);
-        string uid =  z1 + "/" + z2;
-        return uid;
-    }
 
 
 
