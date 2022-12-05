5cf35a33c82cbd0821e86e8a1e841c863f618f91
DawidJerdonek <dawid.jerdonek@gmail.com>
2022-12-05 10:33:15 +0000

    Added Endless Scene to Project RWMPC22-80
    Added all necessary prefabs RWMPC22-81
    
    Created Main Menu which can take player to Story Mode, Endless Mode and Exit RWMPC22-83
    
    Created an Exit feature for Endless mode and fixed level restart RWMPC22-82
    
    Completed Jira Feature : RWMPC22-78

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StoryMode()
+    {
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<PlayerController>().m_cameraMain = Camera.main;
+    }
+
+
+    public void EndlessMode()
+    {
+        SceneManager.LoadScene("EndlessMode");
+        FindObjectOfType<PlayerController>().m_cameraMain = Camera.main;
+    }
+    public void Exit()
+    {
+        Application.Quit();  
+    }
+}
